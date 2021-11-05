3be624bba8ab5624f75776fac6b6f2229b02501d
Reece Smith <5smitr09@solent.ac.uk>
2021-11-05 13:33:57 +0000

    adding beeps eyes

---
+++ Basics/Output/Beeps eyes.py
@@ -0,0 +1,12 @@
+#Added code to display an ASCII robot with dynamic eyes
+
+print("Please type a character")
+
+eye = input()
+
+#Display an ascii art robot
+
+print("##########")
+print(f"#  {eye}  {eye}  #")
+print("#  ----  #")
+print("##########")
